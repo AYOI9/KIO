2c49b72ecd7fde535333e83eede9fb0c8419bde4
maior <maior@DESKTOP-FHGLER9>
2024-12-27 09:43:27 +0200

    Добавьте файлы проекта.

---
+++ KIO/Program.cs
@@ -0,0 +1,22 @@
+﻿
+        int x, y, biggerNumber, cube;
+
+        Console.WriteLine("Введите целое число x:");
+        x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите целое число y:");
+        y = int.Parse(Console.ReadLine());
+
+        if (x > y)
+        {
+            biggerNumber = x;
+        }
+        else
+        {
+            biggerNumber = y;
+        }
+
+        cube = biggerNumber * biggerNumber * biggerNumber;
+
+        Console.WriteLine("Куб большего из чисел: " + cube);
+  
